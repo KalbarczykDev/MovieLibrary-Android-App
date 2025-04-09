401b967e14fc92dc26cdd97ca2501ed06b6bf82c
Oskar Kalbarczyk <oskalbarczyk@gmail.com>
2025-04-09 17:01:52 +0200

    MainActivity sketch

--- app/src/main/java/pl/edu/pja/s27773/filmoteka/view/activity/MainActivity.kt
+++ app/src/main/java/pl/edu/pja/s27773/filmoteka/view/activity/MainActivity.kt
@@ -1,12 +1,12 @@
 package pl.edu.pja.s27773.filmoteka.view.activity
 
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import pl.edu.pja.s27773.filmoteka.R
 import pl.edu.pja.s27773.filmoteka.model.Category
-import pl.edu.pja.s27773.filmoteka.model.Movie
 import pl.edu.pja.s27773.filmoteka.model.Status
-import pl.edu.pja.s27773.filmoteka.model.Title
 import pl.edu.pja.s27773.filmoteka.model.dto.MovieDto
 import pl.edu.pja.s27773.filmoteka.service.MovieService
 import java.time.LocalDate
@@ -18,10 +18,12 @@ class MainActivity : AppCompatActivity() {
     }
 
 
-    fun onAddMovieClick() {
+    fun onAddMovieClick(view: View) {
 
-        val dto = MovieDto(null,"test", LocalDate.now(),Category.MOVIE,Status.NOT_WATCHED,null,null,null)
+        val dto = MovieDto(null, "test", LocalDate.now(), Category.MOVIE, Status.NOT_WATCHED, null, null, null)
 
         MovieService.add(dto)
+
+        Log.d("MainActivity", "Movie added: $dto")
     }
 }
