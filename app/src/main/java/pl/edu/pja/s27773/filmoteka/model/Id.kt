1c0883c9c96b57cf4ff6eb56f932634149f7c222
Oskar Kalbarczyk <oskalbarczyk@gmail.com>
2025-04-11 10:32:28 +0200

    Throw Dedicated Error Instead Of Returning Enums For Validation

--- app/src/main/java/pl/edu/pja/s27773/filmoteka/model/Id.kt
+++ app/src/main/java/pl/edu/pja/s27773/filmoteka/model/Id.kt
@@ -2,17 +2,19 @@ package pl.edu.pja.s27773.filmoteka.model
 
 import pl.edu.pja.s27773.filmoteka.error.IdError
 import pl.edu.pja.s27773.filmoteka.error.AppError
+import pl.edu.pja.s27773.filmoteka.error.AppErrorException
 
 //optymalizacja dla wrapperów
 @JvmInline
 value class Id private constructor(val value: Int) {
     companion object {
-        private fun validate(value: Int): IdError? =
-            if (value <= 0) IdError.INVALID else null
+        private fun validate(value: Int) {
+            if (value <= 0) throw AppErrorException(IdError.Invalid)
+        }
+
 
         fun of(value: Int): Id {
-            val error = validate(value)
-            require(error == null) { error?.stringResKey ?: AppError.DEFAULT }
+            validate(value)
             return Id(value)
         }
     }
